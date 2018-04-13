cfb9a0b46ea68367f5146e5b8b1e83a17711321d
aviralmasson <31753079+aviralmasson@users.noreply.github.com>
2018-04-13 15:12:48 -0400

    first commit

---
+++ Web application.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Web_application;
+using Web_application.Controllers;
+
+namespace Web_application.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Contact()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestClass]
+        public class CarControllerTest
+        {
+            [TestMethod]
+            public void Cars()
+            {
+                // Arrange
+                Car controller = new Car();
+
+                // Act
+                ViewResult result = controller.Index() as ViewResult;
+
+                // Assert
+                Assert.IsNotNull(result);
+            }
+        }
+    }
+}
---
+++ Web application/Controllers/CarController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_application.Controllers
+{
+    public class Car : Controller
+    {
+        public int ID { get; private set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+
+        [NonAction]
+        public List<Car>GetCarList()
+        {
+            return new List<Car>{
+            new Car{
+               ID = 0001,
+               Model = "BMW 530 xdrive",
+               Year = 2017,
+               Price = 60000
+            },
+
+            new Car{
+              ID = 0051,
+               Model = "Dodge Challenger",
+               Year = 2016,
+               Price = 45600
+            },
+
+            new Car{
+              ID = 0060,
+               Model = "Ford Mustang GT",
+               Year = 2017,
+               Price = 72000
+            },
+
+            new Car{
+              ID = 0100,
+               Model = "GMC Denali HQ",
+               Year = 2017,
+               Price = 56000
+            },
+         };
+        }
+
+        // GET: Car
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cars()
+        {
+            var Car = from e in GetCarList()
+                            orderby e.ID
+                            select e;
+            return View(Car);
+        }
+    }
+}
